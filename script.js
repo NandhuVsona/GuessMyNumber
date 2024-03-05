0c8b08055e23ecd59c12b1a3ee525f86f402f42b
Learn Something <131090906+NandhuVsona@users.noreply.github.com>
2024-03-05 21:29:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
 const check = document.getElementById("checkbtn");
 let chance = document.querySelector(".current-score");
-let score = 20;
 let highScore = document.querySelector(".high-score");
 let container = document.querySelector(".container");
 let questionMark = document.getElementById("question");
 let status = document.querySelector(".status");
-const target = randomNumber();
+let target = randomNumber();
+let againBtn = document.getElementById("again");
 document.getElementById("input").focus();
+let score = 20;
 
+againBtn.addEventListener("click", () => {
+  score = 20;
+  target = randomNumber();
+  console.log(target);
+  document.getElementById("input").value = "";
+  questionMark.innerHTML =
+    '<i class="fa-solid fa-question" style="color: #000000"></i>';
+  chance.innerHTML = `Chance Left : ${score}`;
+  container.classList.remove("high");
+  container.classList.remove("low");
+  container.classList.remove("guessed");
+  status.innerHTML = "Start guessing...";
+  container.style.backgroundColor = "black";
+  check.removeAttribute("disabled");
+});
 check.addEventListener("click", () => {
   const inputValue = document.getElementById("input").value;
-  isGuessed(Number(inputValue));
+  if (!inputValue) {
+    status.innerHTML = "Please type a number";
+  } else {
+    isGuessed(Number(inputValue));
+  }
 });
 
 function randomNumber() {
@@ -30,19 +50,24 @@ function isGuessed(number) {
     highScore.innerHTML = `Highscore : ${score}`;
     questionMark.innerHTML = target;
     status.innerHTML = "You Guessed !";
+  } else if (score <= 1) {
+    status.innerHTML = "You loose the game";
+    check.setAttribute("disabled", true);
+    questionMark.innerHTML = target;
   } else if (target > number) {
     container.classList.remove("high");
     let audio = new Audio();
-    audio.src = "./buzzer-or-wrong-answer-20582.mp3";
+    audio.src = "./neg-wrongbuzzer.mp3";
+    audio.playbackRate = 2;
     audio.play();
     score--;
     container.classList.add("low");
-
     status.innerHTML = "Your Guess is Too Low.!";
     chance.innerHTML = `Chance Left : ${score}`;
   } else if (target < number) {
     let audio = new Audio();
-    audio.src = "./buzzer-or-wrong-answer-20582.mp3";
+    audio.src = "./neg-wrongbuzzer.mp3";
+    audio.playbackRate = 2;
     audio.play();
     score--;
     container.classList.remove("low");
